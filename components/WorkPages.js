82b34e2db7b904747a0139980e4bf208816cfb3c
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-02-28 20:22:43 +0545

    SSG implemented for works pages

---
+++ components/WorkPages.js
@@ -0,0 +1,168 @@
+import {
+  Box,
+  chakra,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  VisuallyHidden,
+  List,
+    ListItem,
+  Link
+} from "@chakra-ui/react";
+
+import { FiGithub, FiActivity } from "react-icons/fi";
+import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { MdLocalShipping } from "react-icons/md";
+
+export default function WorkPages({ title, desc, image, stackList, id, slug, codeLink, liveLink, explain }) {
+  return (
+    <Container maxW={"7xl"}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}
+      >
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"product image"}
+            src={
+              "https://images.unsplash.com/photo-1596516109370-29001ec8ec36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyODE1MDl8MHwxfGFsbHx8fHx8fHx8fDE2Mzg5MzY2MzE&ixlib=rb-1.2.1&q=80&w=1080"
+            }
+            fit={"cover"}
+            align={"center"}
+            w={"100%"}
+            h={{ base: "100%", sm: "400px", lg: "500px" }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={"header"}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "4xl", lg: "5xl" }}
+            >
+              Automatic Watch
+            </Heading>
+            <Text
+              color={useColorModeValue("gray.900", "gray.400")}
+              fontWeight={300}
+              fontSize={"2xl"}
+            >
+              $350.00 USD
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={"column"}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+              />
+            }
+          >
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text fontSize={"lg"}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                maxime modi nam officiis porro, quae, quisquam quos
+                reprehenderit velit? Natus, totam.
+              </Text>
+            </VStack>
+            <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={useColorModeValue("yellow.500", "yellow.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}
+              >
+                motivation
+              </Text>
+
+              <Text fontSize={"lg"}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                aliquid amet at delectus doloribus dolorum expedita hic, ipsum
+                maxime modi nam officiis porro, quae, quisquam quos
+                reprehenderit velit? Natus, totam.
+              </Text>
+            </Box>
+            <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={useColorModeValue("yellow.500", "yellow.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}
+              >
+                Technology Used
+              </Text>
+
+              <List spacing={2}>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Between lugs:
+                  </Text>{" "}
+                  20 mm
+                </ListItem>
+              </List>
+            </Box>
+          </Stack>
+          <Flex>
+            <Link mx={8} isExternal href={codeLink}>
+              <Button
+                rounded={"none"}
+                w={"full"}
+                mx={8}
+                size={"lg"}
+                rightIcon={<FiGithub />}
+                bg={useColorModeValue("gray.900", "gray.50")}
+                color={useColorModeValue("white", "gray.900")}
+                textTransform={"uppercase"}
+                _hover={{
+                  transform: "translateY(2px)",
+                  boxShadow: "lg",
+                }}
+              >
+                Code
+              </Button>
+            </Link>
+            <Link href={liveLink} isExternal mx={8}>
+              <Button
+                rounded={"none"}
+                w={"full"}
+                mx={8}
+                target={"_blank"}
+                size={"lg"}
+                rightIcon={<FiActivity />}
+                bg={useColorModeValue("gray.900", "gray.50")}
+                color={useColorModeValue("white", "gray.900")}
+                textTransform={"uppercase"}
+                _hover={{
+                  transform: "translateY(2px)",
+                  boxShadow: "lg",
+                }}
+              >
+                Live
+              </Button>
+            </Link>
+          </Flex>
+
+          <Stack direction="row" alignItems="center" justifyContent={"center"}>
+            <MdLocalShipping />
+            <Text>Support me</Text>
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
+  );
+}
---
+++ pages/work/[slug].js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Transition from '../../components/Transition';
+import WorkPages from "../../components/WorkPages"
+
+export async function getStaticPaths() {
+    const data = require("../../lib/worksInfo.json");
+    // Get the paths we want to pre-render based on posts
+  const paths = data.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+    const FindOne = (data) => {
+        return data.slug === params.slug;
+    }
+    const data = require("../../lib/worksInfo.json");
+    const slugName = data.filter(FindOne);
+
+  // Pass post data to the page via props
+  return { props: { slugName } };
+}
+
+
+const RenderWork = ({ slugName }) => {
+    const data = slugName[0];
+    return (<Transition>
+      <WorkPages />
+  </Transition>);
+};
+
+export default RenderWork;
