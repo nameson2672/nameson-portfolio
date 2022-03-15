a539722e2846556678b44d85ce8a71c0ebb253c5
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-03-15 08:48:57 +0545

    Blog Card added

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,14 @@
+import BlogCard from "../../components/BlogCard";
+import Transition from "../../components/Transition";
+import Head from "next/head";
 
 const blog = () => {
- 
   return (
     <Transition>
       <Head>
         <title>Nameson Gaudel - Blog</title>
       </Head>
-      
+      <BlogCard />
     </Transition>
   );
 };
