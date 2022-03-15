ede9c1cc53b6442eb0f2d5ed0065018fd4f41884
Nameson Gaudel <namesongaudel.ng@gmail.com>
2022-03-15 17:56:07 +0545

    Hamburger menu added on small screen

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,25 @@
 import NextLink from "next/link";
-import { Box, Button, Container, Avatar, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Avatar,
+  Link,
+  IconButton,
+  useColorModeValue,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerHeader,
+  DrawerContent,
+} from "@chakra-ui/react";
 import ToggleMode from "./ModeSwitch";
-import { DownloadIcon } from "@chakra-ui/icons";
+import { DownloadIcon, HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import img from "../public/card.jpg";
 
 const NavBar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Container
@@ -58,12 +73,73 @@ const NavBar = () => {
                 </Button>
               </NextLink>
             </Box>
+            <IconButton
+              onClick={onOpen}
+              m={["1", "2"]}
+              transition={"ease-in-out"}
+              colorScheme={useColorModeValue("blue", "cyan")}
+              display={["block", "block", "none"]}
+              icon={<HamburgerIcon />}
+            />
             <Box m={["1", "2"]}>
               <ToggleMode />
             </Box>
           </Box>
         </Box>
       </Container>
+      <Drawer size={"full"} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">
+            <IconButton
+              m={["1", "2"]}
+              colorScheme={useColorModeValue("blue", "cyan")}
+              variant="ghost"
+              onClick={onClose}
+              icon={<CloseIcon />}
+            />
+          </DrawerHeader>
+          <DrawerBody display={"flex"} flexDirection={"column"}>
+            <NextLink href="/" passHref scroll={false}>
+              <Button m={["1", "2"]} variant="ghost" onClick={onClose}>
+                About
+              </Button>
+            </NextLink>
+            <NextLink href="/work" passHref scroll={false}>
+              <Button m={["1", "2"]} variant="ghost" onClick={onClose}>
+                Work
+              </Button>
+            </NextLink>
+
+            <Link
+              href="https://drive.google.com/file/d/1_Fu-tg4ReidkPmZbjNah8WjotISmHJSf/view"
+              isExternal
+              passHref
+            >
+              <Button
+                m={["1", "2"]}
+                variant="ghost"
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                width={"full"}
+                onClick={onClose}
+              >
+                Resume
+                <Box mx={"1"}>
+                  <DownloadIcon />
+                </Box>
+              </Button>
+            </Link>
+
+            <NextLink href="/blog" passHref scroll={false}>
+              <Button m={["1", "2"]} variant="ghost" onClick={onClose}>
+                Blog
+              </Button>
+            </NextLink>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
